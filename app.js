70c48140e418aaff98ea38539340af0fb40e836e
shafrinahamed97 <shafrinahamedjob@gmail.com>
2022-09-01 16:09:14 +0600

    Dynamic API data load and search on keyboard enter button

--- app.js
+++ app.js
@@ -45,6 +45,8 @@ const displayPhone =( phones,dataLimit )=>
                     <div class="card-body">
                       <h5 class="card-title">${phone.phone_name}</h5>
                       <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+
+                      <button onclick="loadPhoneDetails('${phone.slug}')"  ="#" class="btn btn-primary">Show Details</button>
                     </div>
                   </div>
 
@@ -67,8 +69,16 @@ const processSearch = (dataLimit) =>{
 // handler search button click
 document.getElementById('btn-search').addEventListener('click',function(){
   // Start loader
-  processSearch(10)
+  processSearch(10);
 });
+
+// search input field enter key handler
+document.getElementById('search-field').addEventListener('keypress' , function(e){
+  if(e.key === 'Enter'){
+    processSearch(10);
+  }
+})
+
 
 
 const toggleSpinner = isLoading =>{
@@ -88,5 +98,12 @@ const toggleSpinner = isLoading =>{
 document.getElementById('btn-show-all').addEventListener('click',function(){
   processSearch();
 })
+
+const loadPhoneDetails =async id =>{
+  const url = `https://openapi.programming-hero.com/api/phone/${id}`;
+  const res = await fetch(url);
+  const data = await res.json();
+  console.log(data.data);
+}
 
 // loadPhone();
